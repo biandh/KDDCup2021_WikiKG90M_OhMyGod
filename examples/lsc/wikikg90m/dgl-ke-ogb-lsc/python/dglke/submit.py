26c00851e761d8840c3bea9b512576f869d74925
biandonghai <biandonghai@baidu.com>
2021-06-10 10:42:52 +0800

    inital code

--- examples/lsc/wikikg90m/dgl-ke-ogb-lsc/python/dglke/submit.py
+++ examples/lsc/wikikg90m/dgl-ke-ogb-lsc/python/dglke/submit.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch as th
+import sys
 from ogb.lsc import WikiKG90MEvaluator
 
 evaluator = WikiKG90MEvaluator()
@@ -7,7 +8,9 @@ evaluator = WikiKG90MEvaluator()
 #input_dict = th.load("test_top10_index_by_train_add_val.pkl")
 #input_dict = th.load("test_top10_index_by_train_by_model_mrr_0.88.pkl")
 #input_dict = th.load("test_top10_index_by_ensemble_model_compIEx_mrr_0.885_and_transE_mrr_0.881.pkl")
-input_dict = th.load("test_top10_index_by_ensemble_model_compIEx_mrr_0.885_and_transE_mrr_0.881_xianyan_by_train.pkl")
+path = sys.argv[1]
+print('path:', path)
+input_dict = th.load(path)
 print(type(input_dict['h,r->t']['t_pred_top10']))
 #input_dict['h,r->t'] = {'t_pred_top10': t_pred_top10}
 print(input_dict['h,r->t'].keys())
